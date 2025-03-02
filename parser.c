a54a1f2cc553fad7e2c64b38f9580d7b65380b76
slopezpereyra <santiagolopezpereyra@gmail.com>
2025-03-02 13:15:51 -0300

    Adaptive buffer sizie

--- parser.c
+++ parser.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define DEFAULT_BUF_SIZE 1024
+#define DEFAULT_BUF_SIZE 1
 
 //
 // Allocate DEFAULT_BUF_SIZE bytes into a buffer and write that many bytes into
@@ -14,10 +14,24 @@ char* read_cmd(void){
   int buf_size = DEFAULT_BUF_SIZE;
   int pos = 0;
   char *buffer = malloc(sizeof(char)*buf_size);
+  int ch;
 
-  size_t bytes_read = fread(buffer, 1, buf_size, stdin);
-  printf("Read %u bytes from stdin\n", bytes_read);
-   //(stdin, )
+  while (( ch = fgetc(stdin)) != EOF ){
+    buffer[pos] = ch;
+
+    if (pos == buf_size - 1){
+      buf_size += sizeof(char) * (buf_size + DEFAULT_BUF_SIZE );
+      void *tmp = realloc(buffer, buf_size);
+      if (tmp == NULL){ 
+        printf("Failed to extend input buffer\n");
+        exit(EXIT_FAILURE);
+      }
+      buffer = tmp;
+    }
+
+    pos++;
+
+  }
   return buffer;
 
 }
@@ -29,15 +43,28 @@ char** parse_cmd(char* cmd){
   // Remove "\n" from end of line in `cmd`, if present.
   int l = strlen (cmd);
   if (l > 0 && cmd [l - 1] == '\n') cmd [l - 1] = '\0';
+
+  int buf_size = DEFAULT_BUF_SIZE;
   // Proceed  to tokenize.
-  char **tokens = malloc(DEFAULT_BUF_SIZE * sizeof(char*));
+  char **tokens = malloc(buf_size * sizeof(char*));
   char *cur_token = strtok(cmd, " ");
 
   unsigned int i = 0;
   while (cur_token != NULL){
     tokens[i] = cur_token;
-    i++;
+
+    if (i == buf_size - 1){
+      buf_size += sizeof(char*) * (buf_size + DEFAULT_BUF_SIZE );
+      void *tmp = realloc(tokens, buf_size);
+      if (tmp == NULL){ 
+        printf("Failed to extend input buffer\n");
+        exit(EXIT_FAILURE);
+      }
+      tokens = tmp;
+    }
+
     cur_token = strtok(NULL, " ");
+    i++;
   } 
 
   return tokens;
